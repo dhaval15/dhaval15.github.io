01388fa4f00b3bf9701be6e5e23054383be575f2
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-15 18:32:49 +0530

    $1

---
+++ precache-manifest.25cdf9db680363fdbee3161b414fe809.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2840ded04a8aa6ab28",
+    "url": "/css/app.bb4ad4a1.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "97b42487b25534491877fd71101c523b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af7eb3002714db9c135a",
+    "url": "/js/about.05416c2e.js"
+  },
+  {
+    "revision": "8f2840ded04a8aa6ab28",
+    "url": "/js/app.8a3e7273.js"
+  },
+  {
+    "revision": "f72ec43bfcedfa8652be",
+    "url": "/js/chunk-vendors.72b156ed.js"
+  },
+  {
+    "revision": "ad56bbfe23b56f6dc3b84b9e1146f770",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
